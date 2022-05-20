ce1bee030aef471dd5927c544ae7bce57b27037c
rifkysetiadji <rifkywikrama.s@gmail.com>
2022-05-21 02:24:07 +0700

    update

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,6 +7,7 @@ export default function handler(req, res) {
     maxAge: 1000 * 60 * 15,
     httpOnly: true,
     domain: ".react-cookie.vercel.app",
+    sameSite: "none",
   };
   res.setHeader(
     "Access-Control-Allow-Origin",
