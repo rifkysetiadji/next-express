18dcb57ae67bb5fa52e77e50a5122cfe6d5583ff
rifkysetiadji <rifkywikrama.s@gmail.com>
2022-05-21 14:13:43 +0700

    ipdite

--- pages/api/get.js
+++ pages/api/get.js
@@ -4,10 +4,13 @@ import { serialize } from "cookie";
 export default function handler(req, res) {
   // res.setCookie
   console.log("cookie", req.cookies);
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin",
+  //   "https://react-cookie.vercel.app"
+  // );
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://react-cookie.vercel.app"
+    "Access-Control-Allow-Headers",
+    "Content-Type,X-Requested-With"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With");
   res.send(req.cookies);
 }
