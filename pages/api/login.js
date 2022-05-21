82fd416faa9b3554791e46fcd3389b92d5d29340
rifkysetiadji <rifkywikrama.s@gmail.com>
2022-05-21 14:31:00 +0700

    update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,8 +6,7 @@ export default function handler(req, res) {
   let options = {
     maxAge: 1000 * 60 * 15,
     httpOnly: true,
-    domain: ".next-express-swart.vercel.app",
-    sameSite: "none",
+    // domain: ".next-express-swart.vercel.app",
   };
   // res.setHeader(
   //   "Access-Control-Allow-Origin",
