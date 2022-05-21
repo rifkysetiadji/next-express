0068a867b280634dd134c05e95daf7a28fd19180
rifkysetiadji <rifkywikrama.s@gmail.com>
2022-05-21 14:41:50 +0700

    update

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,7 +6,6 @@ export default function handler(req, res) {
   let options = {
     maxAge: 1000 * 60 * 15,
     httpOnly: true,
-    path: "/asdf",
     // domain: ".next-express-swart.vercel.app",
   };
   // res.setHeader(
